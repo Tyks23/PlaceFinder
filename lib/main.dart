c9a8971dfb92110cb5d5db58924fa5e78ed247c5
kaarelel <70900290+kaarelel@users.noreply.github.com>
2022-05-04 15:26:41 +0300

    PortraitCheck

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'screens/places_screen.dart';
 
 void main() {
@@ -26,6 +27,10 @@ class _LandingScreen extends State<LandingScreen> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return Scaffold(
       appBar: AppBar(
         title: Text("PlaceFinder"),
